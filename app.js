3a01988d357b8e08c18f14084492891f1a240d30
Anren <anren7774@gmail.com>
2024-06-15 18:44:43 +0300

    another test

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 const port = 8080;
 
-app.get('/', (req, res) => res.send('I FORKED BEANSTALK LMFAOOO'));
+app.get('/', (req, res) => res.send('comitting change from anotehr acc'));
 
 app.listen(port);
 console.log(`App running on http://localhost:${port}`);
